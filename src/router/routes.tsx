e7f365c71da1cb32643b442cca7c3ef4770c3aad
Vlatko Jakimoski <jakimoski.vlatko@hotmail.com>
2024-05-31 17:38:50 +0200

    path

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -13,7 +13,7 @@ import FaqsPage from "../pages/FAQs/FaqsPage";
 
 export const routes = createBrowserRouter([
   {
-    path: "/data-hub",
+    path: "",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
