a908e5de2ed70e8ccca883f364654d983ec27768
Vlatko Jakimoski <jakimoski.vlatko@hotmail.com>
2024-06-01 13:39:07 +0200

    data5

--- src/components/TestCard/TestCard.tsx
+++ src/components/TestCard/TestCard.tsx
@@ -1,20 +1,21 @@
 import { Box, Typography } from "@mui/material";
+import image from "../../assets/products-img/Pic placeholder.png";
 
 export default function TestCard() {
   return (
     <Box sx={{ padding: "20px" }}>
       <Box>
         <img
-          src="../../../src/assets/products-img/Pic placeholder.png"
+          src={image}
           alt=""
-          style={{ borderRadius: "20px", width: "300px" }}
+          style={{ borderRadius: "20px", width: "437px" }}
         />
         <Typography sx={{ fontWeight: "600", marginY: "10px" }}>
           Нешто за бледнерот
         </Typography>
-        <Typography sx={{ fontSize: "10px" }}>
-          Секоe семејство има различни потреби. Ние имаме неколку видови на
-          блендери.. <b>Види повеќе</b>
+        <Typography sx={{ fontSize: "16px" }}>
+          Секој корисник и секое семејство има различни потреби. Ние имаме
+          неколку видови на блендери нешто дуго бла... <b>Види повеќе</b>
         </Typography>
       </Box>
     </Box>
